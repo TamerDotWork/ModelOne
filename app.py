2ff902a6572f3ced425b7a746b066bdf0324fd61
Timo Pertilä <timo.pertila@forwardforever.com>
2025-08-23 10:07:27 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/ModelOne')
+
 CORS(app)  # Allow cross-origin requests if needed (optional)
 
 @app.route("/")
