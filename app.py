9ac04b275e73aa579d972fd557fe43f339661bac
Timo Pertilä <timo.pertila@forwardforever.com>
2025-08-23 09:21:26 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def hello_world():
-    """
-    Handles POST requests to the root URL and returns a JSON message.
-    """
+@app.route("/", methods=["GET", "POST"])
+def home():
     if request.method == "POST":
-        return jsonify({"message": "Hello world"}), 200
+        return "This is POST request"
+    return "Hello from Flask!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
